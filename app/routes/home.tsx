24a29b495e2d20f578d565b0855a65d0b5b072c5
Vũ Thành Trung <vuthanhtrungsuper@gmail.com>
2025-07-26 04:18:49 +0000

    fix: update Discord link to the correct URL

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -170,7 +170,7 @@ export default function Page() {
               <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                 Want to chat? Just shoot me a DM{" "}
                 <Link
-                  to="https://links.devtrung.tech/discord"
+                  to="https://links.trunghsgs.edu.vn/discord"
                   className="text-blue-500 hover:underline"
                 >
                   with a direct question on Discord
