b37494d3b2a3a5b6fd023f87322d6a80c611b85e
Vũ Thành Trung <vuthanhtrungsuper@gmail.com>
2025-07-27 14:55:28 +0000

    feat: update hackathon experience description to reflect high school attendance

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -189,7 +189,7 @@ export default function Page() {
                   I like building things
                 </h2>
                 <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                  During my time in university, I attended{" "}
+                  During my time in high school, I attended{" "}
                   {DATA.hackathons.length}+ hackathons. People from around the
                   country would come together and build incredible things in 2-3
                   days. It was eye-opening to see the endless possibilities
