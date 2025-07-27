3d5f0fa7ef38a082ccced23fb4befa5775768d7b
Vũ Thành Trung <vuthanhtrungsuper@gmail.com>
2025-07-27 08:12:39 +0000

    feat: add remark-gfm for GitHub Flavored Markdown support

--- app/data/blog.ts
+++ app/data/blog.ts
@@ -4,6 +4,7 @@ import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
 import rehypeHighlight from "rehype-highlight";
 import rehypeStringify from "rehype-stringify";
+import remarkGfm from "remark-gfm";
 import { z } from "zod";
 
 export type Post = {
@@ -28,6 +29,7 @@ async function markdownToHTML(markdown: string): Promise<string> {
   const p = await unified()
     .use(remarkParse)
     .use(remarkRehype)
+    .use(remarkGfm)
     .use(rehypeHighlight)
     .use(rehypeStringify)
     .process(markdown);
